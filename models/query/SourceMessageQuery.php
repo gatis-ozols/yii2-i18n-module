fdda5e6518b44a577c12173077812063cc3be767
Gatis Ozols <ozols.gatis@gmail.com>
2020-03-18 21:42:07 +0200

    Refactored SourceMessageQuery for code reuse

--- models/query/SourceMessageQuery.php
+++ models/query/SourceMessageQuery.php
@@ -5,10 +5,11 @@ namespace Zelenin\yii\modules\I18n\models\query;
 use Yii;
 use yii\db\ActiveQuery;
 use Zelenin\yii\modules\I18n\models\Message;
+use Zelenin\yii\modules\I18n\models\SourceMessage;
 
 class SourceMessageQuery extends ActiveQuery
 {
-    public function notTranslated()
+    protected static function translatedMessageIdsQuery()
     {
         $messageTableName = Message::tableName();
         $query = Message::find()->select($messageTableName . '.id');
@@ -21,26 +22,22 @@ class SourceMessageQuery extends ActiveQuery
             }
             $i++;
         }
+        return $query;
+    }
+
+    public function notTranslated()
+    {
+        $query = self::translatedMessageIdsQuery();
         $ids = $query->indexBy('id')->all();
-        $this->andWhere(['not in', 'id', array_keys($ids)]);
+        $this->andWhere(['not in', SourceMessage::tableName().'.id', array_keys($ids)]);
         return $this;
     }
 
     public function translated()
     {
-        $messageTableName = Message::tableName();
-        $query = Message::find()->select($messageTableName . '.id');
-        $i = 0;
-        foreach (Yii::$app->getI18n()->languages as $language) {
-            if ($i === 0) {
-                $query->andWhere($messageTableName . '.language = :language and ' . $messageTableName . '.translation is not null', [':language' => $language]);
-            } else {
-                $query->innerJoin($messageTableName . ' t' . $i, 't' . $i . '.id = ' . $messageTableName . '.id and t' . $i . '.language = :language and t' . $i . '.translation is not null', [':language' => $language]);
-            }
-            $i++;
-        }
+        $query = self::translatedMessageIdsQuery();
         $ids = $query->indexBy('id')->all();
-        $this->andWhere(['in', 'id', array_keys($ids)]);
+        $this->andWhere(['in', SourceMessage::tableName().'.id', array_keys($ids)]);
         return $this;
     }
 }
